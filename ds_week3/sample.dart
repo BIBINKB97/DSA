3329760f58781dd9ce50fe5816c2f80afcdfacbe
bibin k b <bibinkochumalayil007@gmail.com>
2023-06-15 14:43:53 +0530

    initial commit

--- ds_week3/sample.dart
+++ ds_week3/sample.dart
@@ -314,6 +314,129 @@
 //   print(obj.contains(20));
 // }
 
+// class TreeNode {
+//   int data;
+//   TreeNode? left;
+//   TreeNode? right;
+//   TreeNode(this.data);
+// }
+
+// class binarySearchTree {
+//   TreeNode? root;
+//   binarySearchTree();
+//   void insert(int data) {
+//     TreeNode? newNode = TreeNode(data);
+//     if (root == null) {
+//       root = newNode;
+//     } else {
+//       TreeNode? currentNode = root;
+
+//       while (true) {
+//         if (data < currentNode!.data) {
+//           if (currentNode.left == null) {
+//             currentNode.left = newNode;
+//           } else {
+//             currentNode = currentNode.left;
+//           }
+//         } else {
+//           if (currentNode.right == null) {
+//             currentNode.right = newNode;
+//           } else {
+//             currentNode = currentNode.right;
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   bool contains(int data) {
+//     TreeNode? currentNode = root;
+//     while (currentNode != null) {
+//       if (currentNode.data == data) {
+//         return true;
+//       } else if (data < currentNode.data) {
+//         currentNode = currentNode.left;
+//       } else {
+//         currentNode = currentNode.right;
+//       }
+//     }
+//     return false;
+//   }
+// }
+
+// void main() {
+//   binarySearchTree obj = binarySearchTree();
+//   obj.insert(10);
+//   obj.insert(20);
+//   obj.insert(30);
+//   obj.insert(45);
+//   obj.insert(5);
+//   print(obj.contains(5));
+// }
+
+// class TreeNode {
+//   int data;
+//   TreeNode? left;
+//   TreeNode? right;
+//   TreeNode(this.data);
+// }
+
+// class binarysearchTree {
+//   TreeNode? root;
+
+//   binarysearchTree();
+
+//   void insert(int data) {
+//     TreeNode newNode = TreeNode(data);
+
+//     if (root == null) {
+//       root = newNode;
+//     } else {
+//       TreeNode? currentNode = root;
+
+//       while (true) {
+//         if (data < currentNode!.data) {
+//           if (currentNode.left == null) {
+//             currentNode.left = newNode;
+//             break;
+//           } else {
+//             currentNode = currentNode.left;
+//           }
+//         } else {
+//           if (currentNode.right == null) {
+//             currentNode.right = newNode;
+//             break;
+//           } else {
+//             currentNode = currentNode.right;
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   bool contains(int data) {
+//     TreeNode? currentNode = root;
+//     while (currentNode != null) {
+//       if (currentNode.data == data) {
+//         return true;
+//       } else if (data < currentNode.data) {
+//         currentNode = currentNode.left;
+//       } else {
+//         currentNode = currentNode.right;
+//       }
+//     }
+//     return false;
+//   }
+// }
+
+// void main() {
+//   binarysearchTree object = binarysearchTree();
+//   object.insert(10);
+//   object.insert(11);
+//   object.insert(14);
+//   print(object.contains(1));
+// }
+
 class TreeNode {
   int data;
   TreeNode? left;
@@ -324,6 +447,7 @@ class TreeNode {
 class binarySearchTree {
   TreeNode? root;
   binarySearchTree();
+
   void insert(int data) {
     TreeNode? newNode = TreeNode(data);
     if (root == null) {
@@ -335,12 +459,14 @@ class binarySearchTree {
         if (data < currentNode!.data) {
           if (currentNode.left == null) {
             currentNode.left = newNode;
+            break;
           } else {
             currentNode = currentNode.left;
           }
         } else {
           if (currentNode.right == null) {
             currentNode.right = newNode;
+            break;
           } else {
             currentNode = currentNode.right;
           }
@@ -351,6 +477,32 @@ class binarySearchTree {
 
   bool contains(int data) {
     TreeNode? currentNode = root;
-    
+    while (currentNode != null) {
+      if (currentNode.data == data) {
+        return true;
+      } else if (data < currentNode.data) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
   }
+
+   void display(TreeNode? node) {
+    if (node != null) {
+      display(node.left);
+      print(node.data);
+      display(node.right);
+    }
+  }
+}
+
+void main() {
+  binarySearchTree object = binarySearchTree();
+  object.insert(10);
+  object.insert(11);
+  object.insert(40);
+  print(object.contains(1));
+  object.display(object.root);
 }
